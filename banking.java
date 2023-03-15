02195a4871500472de6db33a159d756b35a25a82
Cloud User <ec2-user@ip-172-31-3-118.ap-south-1.compute.internal>
2023-03-15 13:23:08 +0000

    updateing

---
+++ banking.java
@@ -0,0 +1,8 @@
+How to connect to server via terminal?
+Connect to servers in Terminal on Mac
+In the Terminal app on your Mac, choose Shell > New Remote Connection.
+Select a protocol in the Service list.
+Select a shared server in the Server list.
+In the User field, enter a user name, then click Connect.
+
+Connect to servers in Terminal on Mac - Apple Su
---
+++ builds.java
@@ -0,0 +1,5 @@
+With Red Hat Enterprise Linux (RHEL) 8, two major versions of Java will be supported: Java 8 and Java 11. In this article, I’ll refer to Java 8 as JDK (Java Development Kit) 8 since we are focusing on the development aspect of using Java. JDK 8 and JDK 11 refer to Red Hat builds of OpenJDK 8 and OpenJDK 11 respectively.
+
+Through this article, you’ll learn how to install and run simple Java applications on RHEL 8, how to switch between two parallel installed major JDK versions via alternatives and how to select one of the two JDKs on a per-application basis.
+
+TL; DR
